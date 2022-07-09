6c37fdd7367b60d05fc42ee22e840e31c8cd4fd8
ShingoTanikawa <shingo.tanikawa@o-n.co.jp>
2022-07-09 12:36:35 +0900

    appendix

--- ts2/appendix/ex1.ts
+++ ts2/appendix/ex1.ts
@@ -2,7 +2,7 @@
 const log = (re: RegExp, s: string) => console.log(re.test(s), re, s);
 
 //
-let re1 = /do+g/;
+const re1 = /do+g/;
 log(re1, 'hounddog');
 log(re1, 'badge');
 log(re1, 'hotdog');
@@ -10,7 +10,7 @@ log(re1, 'doofus');
 log(re1, 'doogie');
 log(re1, 'Doogie');
 
-let re2 = /car*t/;
+const re2 = /car*t/;
 log(re2, 'carted');
 log(re2, 'carrot');
 log(re2, 'cat');
--- ts2/appendix/ex2.ts
+++ ts2/appendix/ex2.ts
@@ -2,17 +2,17 @@
 const log = (re: RegExp, s: string) => console.log(re.test(s), re, s);
 
 //
-let re1 = /ba+/;
+const re1 = /ba+/;
 log(re1, 'ba');
 log(re1, 'baaa');
 log(re1, 'rhumba');
 log(re1, 'babar');
 
-let re2 = /ba(na){2}/;
+const re2 = /ba(na){2}/;
 log(re2, 'banana');
 log(re2, 'semi-banana');
 log(re2, 'bananarama');
 
-let re3 = /dogs? and cats?( and chikens?)?/;
+const re3 = /dogs? and cats?( and chikens?)?/;
 log(re3, 'dogs and cats and chikens');
 log(re3, 'dog and cat');
